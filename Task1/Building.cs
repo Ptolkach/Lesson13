3a9dc6d3bf63adacaaed7135e7c469f558eecd3c
Pavel Tolkach <pb.tolkach@outlook.com>
2022-10-28 17:20:21 +0300

    Добавьте файлы проекта.

---
+++ Task1/Building.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Building
+    {
+        string address;
+        int lenght;
+        int width;
+        int height;
+
+        string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                if (value == "")
+                    address = "Адрес еще не назначен";
+                else
+                    address = value;
+            }
+        }
+        int Lenght
+        {
+            get
+            {
+                return lenght;
+            }
+            set
+            {
+                if (value < 1)
+                    lenght = 1;
+                else
+                    lenght = value;
+            }
+        }
+        int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+            }
+        }
+        int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+        public Building(string address, int lenght, int width, int height)
+        {
+            Address = address;
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+        }
+        public string Print()
+        {
+            return $"Адрес: {address}; Длина здания: {lenght}; Ширина здания: {width}; Высота здания: {height}";
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building B = new Building("Moscow, Red Square, 1", 725, 730, 20);
+            Console.WriteLine(B.Print());
+
+            MultiBuilding MB = new MultiBuilding("SPB, Oblastnaya,1", 990, 16, 75, 25);
+            Console.WriteLine(MB.Print());
+
+            Console.ReadKey();
+
+        }
+    }
+}
